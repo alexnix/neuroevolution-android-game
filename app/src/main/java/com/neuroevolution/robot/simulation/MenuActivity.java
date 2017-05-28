23c0b7306b9183034e7a21bd4ba948f8599eb7d2
Alexandru <alex2august@gmail.com>
2017-05-28 09:23:37 +0300

    improved menu design

--- app/src/main/java/com/neuroevolution/robot/simulation/MenuActivity.java
+++ app/src/main/java/com/neuroevolution/robot/simulation/MenuActivity.java
@@ -1,9 +1,13 @@
 package com.neuroevolution.robot.simulation;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -15,6 +19,16 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        TextView b1 = (TextView) findViewById(R.id.button1);
+        TextView b2 = (TextView) findViewById(R.id.button2);
+        TextView b3 = (TextView) findViewById(R.id.button3);
+
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/display_font.otf");
+
+        b1.setTypeface(tf);
+        b2.setTypeface(tf);
+        b3.setTypeface(tf);
     }
 
     public void antreneaza(View view) {
@@ -29,7 +43,7 @@ public class MenuActivity extends AppCompatActivity {
     }
 
     public void control_manual(View view) {
-        Intent i = new Intent(this, ManualControlActivity.class);
+        Intent i = new Intent(this, DeviceList_Activity.class);
         i.putExtra(NEXT_ACTIVITY, MANUAL_CONTROL_ACTIVITY);
         startActivity(i);
     }
